5b6d8608ae1aafecae2d8ab5bb745f71c334d5bb
SHRADDHAK2017 <skulkarni@my365.bellevue.edu>
2023-03-16 18:07:22 -0500

    Commit HelloWorld assingment

---
+++ Module1/HelloWorldApp/KulkarniHelloWorld.java
@@ -0,0 +1,13 @@
+  /* Module1: HelloWorldApp Date: 03/16/2023
+	Simple Java printf Hello World program.
+	Using string variable for firstName and lastName
+	Used printf to get the suggested output */
+
+public class KulkarniHelloWorld {
+  public static void main(String[] args) {
+    String firstName = "Shraddha";
+    String lastName = "Kulkarni";
+    System.out.printf("Hello World from %s %S %n", firstName, lastName);
+    /* Printf output: Hello World from Shraddha Kulkarni. */
+  }
+}
